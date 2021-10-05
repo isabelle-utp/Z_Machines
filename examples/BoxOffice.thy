2674f762b626830f8d7c947b97d797ad8a1c25eb
Simon Foster <simon.foster@york.ac.uk>
2021-10-05 15:30:48 +0100

    Added comments for the theater box office example

--- examples/BoxOffice.thy
+++ examples/BoxOffice.thy
@@ -4,41 +4,55 @@ theory BoxOffice
   imports "Z_Machines.Z_Machine"
 begin
 
-type_synonym SEAT = integer
-type_synonym CUSTOMER = String.literal
+subsection \<open> Types \<close>
+
+type_synonym seat = integer
+type_synonym customer = String.literal
 
 consts 
-  initalloc :: "SEAT set"
-  SEAT      :: "SEAT set"
-  CUSTOMER  :: "CUSTOMER set"
+  initalloc :: "seat set"
+  SEAT      :: "seat set"
+  CUSTOMER  :: "customer set"
 
-schema BoxOffice = 
-  seating :: "SEAT set"
-  sold :: "SEAT \<Zpfun> CUSTOMER"
-  where "pdom(sold) \<subseteq> seating"
+subsection \<open> State Space \<close>
 
-record_default BoxOffice
+zstore BoxOffice = 
+  \<comment> \<open> The seats available for purchase \<close>
+  seating :: "seat set"
+  \<comment> \<open> Record of seat sales \<close>
+  sold :: "seat \<Zpfun> customer"
+  where dom_sold: "dom(sold) \<subseteq> seating"
+
+subsection \<open> Operations \<close>
+
+text \<open> Purchase of a seat by a customer \<close>
 
 zoperation Purchase0 =
   over BoxOffice
   params s\<in>SEAT c\<in>CUSTOMER
-  pre "s \<notin> pdom(sold)"
+  pre "s \<notin> dom(sold)"
   update "[sold \<leadsto> sold \<oplus> {s \<mapsto> c}]"
+
+text \<open> Return of a seat by a customer \<close>
 
 zoperation Return0 =
   over BoxOffice
   params s\<in>SEAT c\<in>CUSTOMER
-  pre "s \<in> pdom(sold) \<and> c = sold s"
+  pre "s \<in> dom(sold) \<and> c = sold s"
   update "[sold \<leadsto> {s} \<Zndres> sold]" 
+
+subsection \<open> Machine and Animation \<close>
 
 zmachine BoxOfficeProc =
   init "[seating \<leadsto> initalloc, sold \<leadsto> {\<mapsto>}]"
   operations Purchase0 Return0
 
+text \<open> We define finite sets for each of the given sets to allow animation. \<close>
+
 def_consts 
   initalloc = "SEAT"
-  SEAT = "set [0,1,2,3]"
-  CUSTOMER = "set [STR ''Simon'', STR ''Jim'', STR ''Christian'']"
+  SEAT = "{0,1,2,3}"
+  CUSTOMER = "{STR ''Simon'', STR ''Jim'', STR ''Christian''}"
 
 animate BoxOfficeProc
 
