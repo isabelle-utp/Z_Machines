c5dea6cdf413787e520f78a8121894c0c0566ee0
Simon Foster <simon.foster@york.ac.uk>
2023-01-30 17:07:33 +0000

    Preliminary work on a new animator

--- examples/Incubator.thy
+++ examples/Incubator.thy
@@ -35,8 +35,7 @@ lemma Decrement_correct: "\<^bold>{IncubatorMonitor_inv\<^bold>} Decrement() \<^
 
 zoperation GetTemp =
   over IncubatorMonitor
-  params currentTemp \<in> TEMP
-  where "temp = currentTemp"
+  params currentTemp \<in> "{temp}"
 
 lemma GetTemp_correct: "\<^bold>{IncubatorMonitor_inv\<^bold>} GetTemp v \<^bold>{IncubatorMonitor_inv\<^bold>}"
   by zpog_full
@@ -45,7 +44,30 @@ zmachine Incubator =
   init "[temp \<leadsto> 20]"
   operations Increment Decrement GetTemp
 
-animate Incubator
+instantiation Incubator_chan :: "show"
+begin
+
+fun show_Incubator_chan :: "Incubator_chan \<Rightarrow> String.literal" where
+"show_Incubator_chan (increment_C x) = STR ''Increment '' + show x" |
+"show_Incubator_chan (decrement_C x) = STR ''Decrement '' + show x" |
+"show_Incubator_chan (getTemp_C x) = STR ''GetTemp '' + show x"
+
+instance ..
+
+end
+
+definition z_machine_main2 :: "(('e, 's) htree) list \<Rightarrow> ('e::show, 's) htree" where
+"z_machine_main2 Ops = foldr (\<box>) Ops Stop"
+
+definition z_machine2 :: "('s::default) subst \<Rightarrow> ('s \<Rightarrow> bool) \<Rightarrow> (('e::show, 's) htree) list \<Rightarrow> 'e process" where
+[code_unfold]: "z_machine2 Init Inv Ops = process Init (loop (z_machine_main2 Ops))"
+
+definition "Incubator2 =
+z_machine2 [temp \<leadsto> 20] (\<lambda>\<s>. True)
+ [zop_event increment Increment_type Increment, zop_event decrement Decrement_type Decrement,
+  zop_event getTemp GetTemp_type GetTemp]"
+
+animate Incubator2
 
 end
   
