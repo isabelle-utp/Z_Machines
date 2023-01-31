eb1eb21a23ff971d29c69f5d7bd2671159b13e90
Simon Foster <simon.foster@york.ac.uk>
2023-01-31 16:01:14 +0000

    Remove Haskell Show, as it's now in Z Toolkit

--- Z_Machine.thy
+++ Z_Machine.thy
@@ -1,6 +1,6 @@
 theory Z_Machine
   imports Z_Operations "ITree_Simulation.ITree_Simulation" "Z_Toolkit.Z_Toolkit" 
-    "HOL-Library.Code_Target_Numeral" "Explorer.Explorer" "Haskell_Show"
+    "HOL-Library.Code_Target_Numeral" "Explorer.Explorer"
   keywords "zmachine" "zoperation" :: "thy_decl_block"
     and "over" "init" "invariant" "operations" "params" "pre" "update" "\<in>"
 begin
