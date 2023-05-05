407e6c5a0eaf284be0cf0184da0215d41c7bc034
Simon Foster <simon.foster@york.ac.uk>
2023-05-05 16:23:25 +0100

    Implemented promotion. There is now an additional way of writing operations that promote another operation using a lens. There is an example in "TrivialPursuit.thy"

--- examples/TrivialPursuit.thy
+++ examples/TrivialPursuit.thy
@@ -2,53 +2,40 @@ theory TrivialPursuit
   imports "Z_Machines.Z_Machine"
 begin
 
-lit_vars
-
 enumtype Player = one | two | three | four | five | six
 
 enumtype Colour = blue | pink | yellow | brown | green | orange
 
+declare UNIV_enum [code_unfold]
+
+declare enum_Colour [code_unfold]
+declare enum_Player [code_unfold]
+
 definition Colour :: "Colour set" where
 "Colour = UNIV"
 
-alphabet LocalScore =
+definition Player :: "Player set" where
+"Player = UNIV"
+
+zstore LocalScore =
   s :: "Colour set"
 
-record_default LocalScore
-
-alphabet GlobalScore =
+zstore GlobalScore =
   score :: "Player \<Zpfun> LocalScore"
-
-record_default GlobalScore
 
 zoperation AnswerLocal =
   params c \<in> Colour
-  pre "c \<in> s"
+  pre "c \<notin> s"
   update "[s \<leadsto> s \<union> {c}]"
 
-chantype chan1 =
-  answer1 :: "Colour"
+zoperation AnswerGlobal =
+  promote AnswerLocal in score
 
-chantype chan =
-  answer :: "Player \<times> Colour"
-
-definition "Answer = operation answer1 AnswerLocal"
-
-definition AnswerGlobal :: "(chan, GlobalScore) htree" where
-[code_unfold]: "AnswerGlobal = operation answer (promote_op (\<lambda> p. (score[p])\<^sub>v) AnswerLocal)"
-
-definition "TrivialPursuit = process [\<leadsto>] (loop (Answer \<box> Stop))"
-
-lemma "AnswerGlobal = undefined"
-  apply (simp add: AnswerGlobal_def operation_def)
-  oops
-
-declare operation_def [code_unfold]
-declare wp [code_unfold]
-declare unrest [code_unfold]
-
-export_code TrivialPursuit in Haskell module_name TrivialPursuit (string_classes)
-
-term "AnswerLocal c \<Up>\<Up> score[p]"
+zmachine TrivialPursuit =
+  over GlobalScore
+  init "[score \<leadsto> (\<lambda> p\<in>Player \<bullet> \<lblot> s \<leadsto> {} \<rblot>)]"
+  operations AnswerGlobal
+ 
+animate TrivialPursuit
 
 end
