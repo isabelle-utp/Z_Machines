8caec9ef9b325dcbc81ad6860ec77896b00d228f
Simon Foster <simon.foster@york.ac.uk>
2023-05-22 21:30:00 +0100

    Added command for deadlock check, and code to support checking for particular events

--- Z_Testing.thy
+++ Z_Testing.thy
@@ -2,13 +2,14 @@ section \<open> Testing Z-Machines \<close>
 
 theory Z_Testing
   imports Z_Animator
+  keywords "check_deadlock" :: "thy_defn"
 begin
 
 code_identifier
   code_module Z_Testing \<rightharpoonup> (Haskell) Interaction_Trees
 
 generate_file \<open>code/animate/ZTesting.hs\<close> = \<open>
-module ZTesting (itree_rev_traces, dlock_traces, test_dlock) where
+module ZTesting (itree_rev_traces, dlock_traces, test_dlock, test_event) where
 import Interaction_Trees;
 import Prelude;
 import System.IO;
@@ -40,12 +41,26 @@ itree_rev_traces :: Int -> Itree e s -> [[IEvt e]]
 itree_rev_traces n p = map fst ((iterate expand_itrees [([], p)]) !! (n+1))
 
 dlock_traces :: Int -> Itree e s -> [[e]]
-dlock_traces n p = map (reverse . map getEvt . filter isEvt) (filter (isDlock . head) (itree_rev_traces n p))
+dlock_traces n p = map (reverse . map getEvt . filter isEvt) (filter (isDlock . head) trs)
+  where trs = itree_rev_traces n p
+
+event_traces :: Int -> (e -> Bool) -> Itree e s -> [[e]]
+event_traces n t p = map (reverse . map getEvt . filter isEvt) (filter ((\e -> isEvt e && t (getEvt e)) . head) trs)
+  where trs = itree_rev_traces n p
+
+test_event :: Show e => Int -> (e -> Bool) -> Itree e s -> IO ()
+test_event n t p = do { case trs of
+                        [] -> putStrLn ("No matching traces found of length <= " ++ show n ++ ".")
+                        (tr : _) -> putStrLn ("Trace found: " ++ show tr ++ ".")
+                    }
+  where trs = event_traces n t p
 
 test_dlock :: Show e => Int -> Itree e s -> IO ()
-test_dlock n p = case dlock_traces n p of
-                   [] -> putStrLn ("No deadlocking traces found of length <= " ++ show n ++ ".")
-                   (tr : _) -> putStrLn ("Deadlocks after: " ++ show tr ++ ".")
+test_dlock n p = do { case trs of
+                        [] -> putStrLn ("No deadlocking traces found of length <= " ++ show n ++ ".")
+                        (tr : _) -> putStrLn ("Deadlocks after: " ++ show tr ++ ".")
+                    }
+  where trs = dlock_traces n p
 \<close>
 
 ML \<open>
@@ -53,32 +68,38 @@ ML \<open>
 structure Z_Machine_Testing =
 struct
 
+fun test_files_cp ghc tmp = 
+  "(fn path => let open Isabelle_System; val path' = Path.append path (Path.make [\"code\", \"animate\"])" ^
+  " in writeln \"Compiling test...\"; bash (\"cd \" ^ Path.implode path' ^ \"; " ^ ghc ^ " ZTest.hs >> /dev/null; ./ZTest\") ; copy_dir path' (Path.explode \"" ^ tmp ^ "\") end)";
+
+
 fun dlock_test_file model thy =
   "module Main where \n" ^
   "import ZTesting; \n" ^
   "import " ^ thy ^ "; \n" ^
   "main = test_dlock 10 " ^ Z_Machine_Animator.firstLower model
 
-fun dlock_test_files_cp ghc tmp = 
-  "(fn path => let open Isabelle_System; val path' = Path.append path (Path.make [\"code\", \"animate\"])" ^
-  " in writeln \"Compiling deadlock test...\"; bash (\"cd \" ^ Path.implode path' ^ \"; " ^ ghc ^ " DlockTest.hs >> /dev/null; ./DlockTest\") ; copy_dir path' (Path.explode \"" ^ tmp ^ "\") end)";
+fun event_test_file model chan thy =
+  "module Main where \n" ^
+  "import ZTesting; \n" ^
+  "import " ^ thy ^ "; \n" ^
+  "main = test_event 10 is_" ^ chan ^ " " ^ Z_Machine_Animator.firstLower model
 
-
-fun prep_testing model thy ctx =
+fun prep_testing test_file ctx =
   let open Generated_Files; 
       val (tmp, thy') = Z_Machine_Animator.simulator_setup (Local_Theory.exit_global ctx);
       val ctx' = Named_Target.theory_init thy'
       val ghc = getenv "ISABELLE_GHC"
       val _ = if (ghc = "") then error "GHC is not set up. Please set the environment variable ISABELLE_GHC." else ()
   in
-  generate_file (Path.binding0 (Path.make ["code", "animate", "DlockTest.hs"]), (Input.string (dlock_test_file model thy))) ctx' |>
+  generate_file (Path.binding0 (Path.make ["code", "animate", "ZTest.hs"]), (Input.string test_file)) ctx' |>
   (fn ctx' => 
     let val _ = compile_generated_files 
                  ctx'
                  [([], (Local_Theory.exit_global ctx')), ([Path.binding0 (Path.make ["code", "animate", "ZTesting.hs"])], @{theory})] 
-                 [] [([Path.binding0 (Path.make ["code", "animate", "DlockTest"])], SOME true)]
+                 [] [([Path.binding0 (Path.make ["code", "animate", "ZTest"])], SOME true)]
                  (Path.binding0 (Path.make []))
-                 (Input.string (dlock_test_files_cp ghc (Path.implode tmp)))
+                 (Input.string (test_files_cp ghc (Path.implode tmp)))
     in ctx' end)
   end
 
@@ -86,12 +107,30 @@ fun dlock_test model thy =
   let val ctx = Named_Target.theory_init thy
       val ctx' =
         (Code_Target.export_code true [Code.read_const (Local_Theory.exit_global ctx) model] [((("Haskell", ""), SOME ({physical = false}, (Path.explode "animate", Position.none))), [])] ctx)
-        |> prep_testing model (Context.theory_name thy)
+        |> prep_testing (dlock_test_file model (Context.theory_name thy))
   in thy
   end 
 
+fun event_test model chan thy =
+  let val ctx = Named_Target.theory_init thy
+      val ctx' =
+        (Code_Target.export_code true [Code.read_const (Local_Theory.exit_global ctx) model] [((("Haskell", ""), SOME ({physical = false}, (Path.explode "animate", Position.none))), [])] ctx)
+        |> prep_testing (event_test_file model chan (Context.theory_name thy))
+  in thy
+  end 
+
+
 end
 
+\<close>
+
+
+ML \<open>
+  Outer_Syntax.command @{command_keyword check_deadlock} "check deadlock in a Z Machine"
+  (Parse.name 
+   >> (fn model => 
+        Toplevel.theory 
+        (fn thy => let val anim = Z_Machine_Testing.dlock_test model thy in thy end)));
 \<close>
 
 
